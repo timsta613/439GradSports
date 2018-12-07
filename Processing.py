3eaf5d9066b1cefe8f7227b060af8941387a42b2
Jimmy <jimmylee@mit.edu>
2018-12-06 21:54:14 -0500

    updated 3pt_speeds.csv with header and no repeated column

--- Processing.py
+++ Processing.py
@@ -11,10 +11,11 @@ from Utils import *
 # Change these paths to your local instances before running
 dropboxDir = '/home/jimmy/Dropbox (MIT)/6_439 Group Project/' # Path to project directory
 codeDir = '/home/jimmy/Desktop/439GradSports/' # Path to project code
-
+baseDir = dropboxDir
 
 def main():
-	extractSpecificMovementData(movementDataSample)
+	#extractSpecificMovementData(movementDataSample)
+	#extractRandomMovementData(637)
 	writeSpeedCSV()
 
 
@@ -90,91 +91,95 @@ def writeSpeedCSV():
 	os.chdir(codeDir)
 	gameIDs = [x.split('.')[0] for x in os.listdir('./data/movement/')] # Combs through every movement data file in data directory
 	outfile = '/home/jimmy/Desktop/test/3pt_speeds.csv'
-
+	i = 0
 	for gameID in gameIDs:
+		i+= 1
+		print(i)
 		print(gameID)
 		Data = readJson(gameID)
+		try:
+			all3pts = get_all_3pt(Data)
+			for shot in all3pts:
+				shooterID = shot['shooterID']
+				eventID = shot['eventID']
+				movement = get_movements(Data, eventID, shooterID)
+				if movement is None:
+					continue
 
-		all3pts = get_all_3pt(Data)
+				t_shot = get_shot_index(movement) # Time in frames from beginning of the event
+				if t_shot is None:
+					continue
 
-		for shot in all3pts:
-			shooterID = shot['shooterID']
-			eventID = shot['eventID']
-			movement = get_movements(Data, eventID, shooterID)
-			if movement is None:
-				continue
+				t_catch = get_catch_index(movement) # Same
 
-			t_shot = get_shot_index(movement) # Time in frames from beginning of the event
-			if t_shot is None:
-				continue
+				t_min_before_highest = get_shot_index_old(movement)
 
-			t_catch = get_catch_index(movement) # Same
+				if t_catch == t_shot:
+					print('Event {} - Shooter {} caught ball at {} and shot at {}'.format(
+						eventID, shooterID, t_catch, t_shot))
+					continue
 
-			t_min_before_highest = get_shot_index_old(movement)
+				is_cns = is_catch_and_shoot(movement, shooterID)
 
-			if t_catch == t_shot:
-				print('Event {} - Shooter {} caught ball at {} and shot at {}'.format(
-					eventID, shooterID, t_catch, t_shot))
-				continue
+				t_with_ball = (t_shot - t_catch) / 25
 
-			is_cns = is_catch_and_shoot(movement, shooterID)
+				v_with_ball = shooter_velocity_between_frames(movement, shooterID, t_catch, t_shot)
 
-			t_with_ball = (t_shot - t_catch) / 25
-
-			v_with_ball = shooter_velocity_between_frames(movement, shooterID, t_catch, t_shot)
-
-			v_before_shot = dict()
-			# Find average velocity over n secs before shot for n = 0.5, 1, 1.5, ... 5]
-			for n in [x / 2.0 for x in range(1, 11)]:
-				v_before_shot[n] = None
-				if n < t_with_ball:
-					v_before_shot[n] = shooter_velocity_between_frames(
-						movement, shooterID, int(t_shot - 25*n), t_shot)
-				# print('{} - {}'.format(n, v_before_shot[n]))
+				v_before_shot = dict()
+				# Find average velocity over n secs before shot for n = 0.5, 1, 1.5, ... 5]
+				for n in [x / 2.0 for x in range(1, 11)]:
+					v_before_shot[n] = None
+					if n < t_with_ball:
+						v_before_shot[n] = shooter_velocity_between_frames(
+							movement, shooterID, int(t_shot - 25*n), t_shot)
+					# print('{} - {}'.format(n, v_before_shot[n]))
 
 
-			v_before_catch = dict()
-			for n in [x / 2.0 for x in range(1, 11)]:
-				v_before_catch[n] = None
-				start_frame = int(t_catch - 25*n)
-				if start_frame > 0:
-					v_before_catch[n] = shooter_velocity_between_frames(
-						movement, shooterID, start_frame, t_catch)
-			shot_clock = get_shot_clock_at_frame(Data, eventID, t_shot)
-			row = list()
-			row.append(gameID)
-			row.append(shooterID)
-			row.append(eventID)
-			row.append(shot_clock)
-			row.append(is_cns)
-			row.append(3*movement[0])
-			row.append(t_with_ball)
-			row.append(v_with_ball)
-			row.append(shooter_dist_at_time(movement,t_shot))
-			row.append(ball_angle(movement, t_shot))
-			row.append(shooter_move_angle(movement, shooterID, t_catch, t_shot))
-			row.append(shooter_move_tobasket(movement,shooterID, t_catch, t_shot))
-			try:
-				row.append(closest_defender_dist(movement, t_shot,Data, eventID, shooterID)[0])                
-			except:
-				row.append(-100)                                
-			try:
-				row.append(closest_defender_dist(movement, t_shot,Data, eventID, shooterID)[1])                
-			except:
-				row.append(-100)                                
-			try:
-				row.append(closest_defender_velocity(movement, t_catch, t_shot,Data, eventID, shooterID))                
-			except:
-				row.append(-100)                                            #row.append(closest_defender_dist(movement, t_shot,Data, eventID, shooterID)[1])
-			#row.append(closest_defender_velocity(movement,t_catch,t_shot, Data, eventID, shooterID))
-			row.append(t_with_ball)
-			row.append(v_with_ball)
+				v_before_catch = dict()
+				for n in [x / 2.0 for x in range(1, 11)]:
+					v_before_catch[n] = None
+					start_frame = int(t_catch - 25*n)
+					if start_frame > 0:
+						v_before_catch[n] = shooter_velocity_between_frames(
+							movement, shooterID, start_frame, t_catch)
+				shot_clock = get_shot_clock_at_frame(Data, eventID, t_shot)
+				row = list()
+				row.append(gameID)
+				row.append(shooterID)
+				row.append(eventID)
+				row.append(shot_clock)
+				row.append(is_cns)
+				row.append(3*movement[0])
+				row.append(t_with_ball)
+				row.append(v_with_ball)
+				row.append(shooter_dist_at_time(movement,t_shot))
+				row.append(ball_angle(movement, t_shot))
+				row.append(shooter_move_angle(movement, shooterID, t_catch, t_shot))
+				row.append(shooter_move_tobasket(movement,shooterID, t_catch, t_shot))
+				try:
+					row.append(closest_defender_dist(movement, t_shot,Data, eventID, shooterID)[0])                
+				except:
+					row.append(-100)                                
+				try:
+					row.append(closest_defender_dist(movement, t_shot,Data, eventID, shooterID)[1])                
+				except:
+					row.append(-100)                                
+				try:
+					row.append(closest_defender_velocity(movement, t_catch, t_shot,Data, eventID, shooterID))                
+				except:
+					row.append(-100)                                            #row.append(closest_defender_dist(movement, t_shot,Data, eventID, shooterID)[1])
+				#row.append(closest_defender_velocity(movement,t_catch,t_shot, Data, eventID, shooterID))
+				
+				#row.append(t_with_ball)
+				#row.append(v_with_ball)
 
-			[row.append(v_before_shot[x]) for x in v_before_shot]
-			[row.append(v_before_catch[x]) for x in v_before_catch]
+				[row.append(v_before_shot[x]) for x in v_before_shot]
+				[row.append(v_before_catch[x]) for x in v_before_catch]
 
-			with open(outfile, 'a+', newline='') as outf:
-				writer = csv.writer(outf)
-				writer.writerow(row)
+				with open(outfile, 'a+', newline='') as outf:
+					writer = csv.writer(outf)
+					writer.writerow(row)
+		except:
+			pass
 if __name__ == '__main__':
 	main()
